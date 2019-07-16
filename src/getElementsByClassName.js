74f9fffc60808079b990b2f8e3b4a6b3a4ba0dad
kaylaTill <kaylatillman319@gmail.com>
2019-07-15 21:31:57 -0700

    Completion of the Review of getElementsByClassName and stringifyJSON recursino

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,13 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
+  var body=document.body
+  var results=[]
+  function getClassElements(element){
+    if(element.classList && element.classList.contains(className)){results.push(element)}
+    if(element.childNodes){element.childNodes.forEach(child=>getClassElements(child))}
+  }
+  getClassElements(body)
+  return results;
   // your code here
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,40 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+  // edge cases: undefined, function, null
+  // base cases: number, string, boolean
+  // arrays: iterate through every element of the array using forEach and recursion function
+  // objects: get object keys/ stringify each key-value pairs
+if(obj===null){
+  return "null";
+}
+if (typeof(obj)==="string") {
+  return `"${obj}"`;
+}
+if (typeof(obj)==="function" ||typeof(obj)==="undefined"){
+  return "";
+}
+
+if(Array.isArray(obj)){
+  let arr=[]
+  for (let i=0; i<obj.length; i++){
+    arr.push(stringifyJSON(obj[i]))
+  }
+  return `[${arr.toString()}]`
+
+}
+if(typeof(obj)==="object"){
+  var newObj = "";
+  var keys = Object.keys(obj);
+
+  keys.forEach(function(element){
+    if (typeof(element)==="undefined"||typeof(element)==="function"||typeof(obj[element])==="undefined"||typeof(obj[element])==="function"){
+      return "";
+    }else newObj += `${stringifyJSON(element)}:${stringifyJSON(obj[element])},`;
+  })
+  return `{${newObj.slice(0,newObj.length-1)}}`;
+}
+else return obj.toString()
+
   // your code goes here
 };
